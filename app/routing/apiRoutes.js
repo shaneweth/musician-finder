403d16edde2990f0246eac6b9e300d8c53a576fb
willow <shaneweth77@gmail.com>
2019-05-06 12:34:35 -0400

    routes are working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,16 @@
-var friends = require("../public/friends");
+var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-}
 
-app.post("/api/friends", function(req, res) {
+
+    app.post("/api/friends", function (req, res) {
 
     friends.push(req.body);
     res.json(true);
 })
+
+}
