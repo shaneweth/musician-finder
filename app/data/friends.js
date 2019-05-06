58e6cdf26a0ab05be4fea34bff785f7214a91579
willow <shaneweth77@gmail.com>
2019-05-06 11:08:21 -0400

    added basic html and connections to server.js

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var musicianArray = [
+    {
+        "name":"bob",
+        "photo":"http://lorempixel.com/400/200/",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    }
+];
+
+module.exports = musicianArray;
