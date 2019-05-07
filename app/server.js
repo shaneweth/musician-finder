b24b31dbed77b099fe3042af8b3a659bf1d50694
willow <shaneweth77@gmail.com>
2019-05-07 17:02:57 -0400

    added listener for heroku

--- app/server.js
+++ app/server.js
@@ -3,7 +3,11 @@ var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
